684a18c049728281f89dcd76375a24c7c5b2298c
Alex <alexander.sartory@gmail.com>
2026-09-16 20:34:05 +0200

    bbb

--- Build/gacha.loader.js
+++ Build/gacha.loader.js
@@ -50,7 +50,7 @@ function createUnityInstance(canvas, config, onProgress = () => { }) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 48490447,
+    wasmFileSize: 48517532,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     onProgress,
